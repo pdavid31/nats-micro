efe6c106845988ae841191baa92b9605d6ac0e09
Paul David <paul.david@magenta.de>
2024-04-25 10:01:11 +0200

    Removed legacy function code for now

--- src/main.rs
+++ src/main.rs
@@ -43,57 +43,12 @@ async fn main() -> Result<(), async_nats::Error> {
     stream_map.insert("svc.math.v1.add", v1_group.endpoint("add").await?);
 
     println!("Starting to listen on {}", address);
-    while let Some((key, request)) = stream_map.next().await {
-        match key {
-            "svc.math.v1.add" => wrapper(services::add, &request).await?,
-            _ => unreachable!(),
-        }
-    }
+    // while let Some((key, request)) = stream_map.next().await {
+    //     match key {
+    //         "svc.math.v1.add" => wrapper(services::add, &request).await?,
+    //         _ => unreachable!(),
+    //     }
+    // }
 
     Ok(())
-}
-
-async fn wrapper<'a, F, I, O>(func: F, request: &'a Request) -> Result<(), async_nats::PublishError>
-where
-    F: FnOnce(I) -> Result<O, Error>,
-    I: TryFrom<&'a Request>,
-    O: TryInto<Bytes>,
-    // we have to make sure that the conversion from input and output to bytes
-    // produces printable errors, which is why require those to implement std::error::Error
-    <I as TryFrom<&'a Request>>::Error: StdError,
-    <O as TryInto<Bytes>>::Error: StdError,
-{
-    let response = internal_caller(func, request).await;
-    request.respond(response).await
-}
-
-async fn internal_caller<'a, F, I, O>(
-    func: F,
-    request: &'a Request,
-) -> Result<Bytes, async_nats::service::error::Error>
-where
-    F: FnOnce(I) -> Result<O, Error>,
-    I: TryFrom<&'a Request>,
-    O: TryInto<Bytes>,
-    // we have to make sure that the conversion from input and output to bytes
-    // produces printable errors, which is why require those to implement std::error::Error
-    <I as TryFrom<&'a Request>>::Error: StdError,
-    <O as TryInto<Bytes>>::Error: StdError,
-{
-    let input = I::try_from(request).map_err(|err| async_nats::service::error::Error {
-        status: format!("failed to deserialize request body: {}", err),
-        code: 400,
-    })?;
-
-    let result = func(input).map_err(|err| async_nats::service::error::Error {
-        status: format!("failed to compute: {}", err),
-        code: 500,
-    })?;
-
-    result
-        .try_into()
-        .map_err(|err| async_nats::service::error::Error {
-            status: format!("failed serialize response body: {}", err),
-            code: 500,
-        })
 }

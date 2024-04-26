69d20c28418752ac3d16c4e2169748efaba865dd
Paul David <paul.david@magenta.de>
2024-04-26 15:27:43 +0200

    Updated main.rs

--- nats-microservice-application/src/main.rs
+++ nats-microservice-application/src/main.rs
@@ -5,9 +5,9 @@ extern crate tokio;
 use async_nats::{service::ServiceExt, ConnectOptions};
 use tokio_stream::{StreamExt as _, StreamMap};
 
-use nats_microservice_rs::EndpointWithHandler as _;
+use nats_microservice_rs::HandlerExt;
 
-use crate::services::WeatherHandler;
+use crate::services::{AddHandler, WeatherHandler};
 
 #[tokio::main]
 async fn main() -> Result<(), async_nats::Error> {
@@ -25,29 +25,40 @@ async fn main() -> Result<(), async_nats::Error> {
 
     let client = async_nats::connect_with_options(address, options).await?;
 
-    let service = client
+    let mut stream_map = StreamMap::new();
+
+    let weather_service = client
         .service_builder()
         .description("This is a great weather microservice")
         .queue_group("svc.weather")
         .start("weather", "0.1.0")
         .await?;
 
-    let v1_group = service.group("svc.weather");
+    let math_service = client
+        .service_builder()
+        .description("This is a great math microservice")
+        .queue_group("svc.math")
+        .start("math", "0.1.0")
+        .await?;
 
-    let mut stream_map = StreamMap::new();
+    let add_endpoint = math_service.endpoint("svc.math.v1.add").await?;
+    let add_handler = AddHandler::new();
+    stream_map.insert("svc.math.v1.add", add_endpoint);
 
-    let handler = WeatherHandler::new();
-    let endpoint_handler = v1_group.endpoint("v1").await?.with_handler(handler)?;
-
-    stream_map.insert("svc.weather.v1.add", endpoint_handler);
+    let weather_endpoint = weather_service.endpoint("svc.weather.v1").await?;
+    let weather_handler = WeatherHandler::new();
+    stream_map.insert("svc.weather.v1", weather_endpoint);
 
     println!("Starting to listen on {}", address);
-    while let Some((key, result)) = stream_map.next().await {
-        match result {
-            Ok(_) => {}
-            Err(err) => {
-                println!("encountered error on {}: {}", key, err);
+    while let Some((key, request)) = stream_map.next().await {
+        match key {
+            "svc.math.v1.add" => {
+                add_handler.handle(request).await?;
             }
+            "svc.weather.v1" => {
+                weather_handler.handle(request).await?;
+            }
+            _ => {}
         }
     }
 
